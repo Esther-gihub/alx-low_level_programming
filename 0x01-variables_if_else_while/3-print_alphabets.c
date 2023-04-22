19a153ad27400d64e4529f03c73867b24a588a6f
Esther-gihub <estherowusu18@gmail.com>
2023-04-22 03:52:50 -0700

    fourth c task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This programme prints the alphabets in lowercase and uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+	int q = 65;
+
+	while (n <= 122);
+	{
+		putchar (n);
+	}
+	while (q <= 90);
+	{
+		putchar (q);
+	}
+	putchar ('\n');
+	return (0);
+}
+
