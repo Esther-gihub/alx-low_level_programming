4f3befbf86fe987320a9062911b04a10c577d1f7
Esther-gihub <estherowusu18@gmail.com>
2023-04-26 00:13:44 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * Description: This program adds two integers and returns the result
+ *
+ * Return: Always 0
+ */
+int add(int, int);
+{
+	int num1 = 5;
+	int num2 = 7;
+	{
+		int add( num1, num2);
+	}
+		
+		return (0);
+}
+
+
