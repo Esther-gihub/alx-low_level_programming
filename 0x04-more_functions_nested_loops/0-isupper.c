1f62a68ba87e006f55769825a36970498d035e77
Esther-gihub <estherowusu18@gmail.com>
2023-04-27 22:52:26 -0700

    ctask 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: This function checks for uppercase characters
+ * Return: Always 0
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
