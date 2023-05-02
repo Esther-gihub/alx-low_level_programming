08e4b616c63ce84e0bec1150d574acf58e5ab6b9
Esther-gihub <estherowusu18@gmail.com>
2023-05-02 08:26:03 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_strlen - This program returns the length of
+*a string
+*@s: string to be checked
+*Return: The length of string
+*/
+int _strlen(char *s)
+}
+
+	int string_length = 0;
+
+	while (s[string_length])
+		++string_length;
+	return (string_lenth);
+}
+
