13551b771676393ff57c3cf3e933af8636003461
Esther-gihub <estherowusu18@gmail.com>
2023-05-04 07:36:09 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcat - This function appends a string, overides and terminates
+ *null byte.
+ *@dest: destination string pointer
+ *@src: source string pointer
+ * Returns: pointer to destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p)
+	{
+		p + 1;
+	}
+	while (*src)
+	{
+		*p + 1 = *src + 1;
+	}
+
+	*p = '\0';
+
+		return (dest);
+}
+
