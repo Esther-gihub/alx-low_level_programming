b96b0e5e2cde870ecff68aca505b1de08ef33154
Esther-gihub <estherowusu18@gmail.com>
2023-05-11 00:36:51 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *is_prime_number - This function ouputs a prime number
+  *@n: value in question
+  *
+  *Return: 1 if number is a prime number or otherwise
+  */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+
+	for (int e = 2; e * e <= n; e++)
+	{
+	if (n % e == 0)
+	}
+	return (0);
+
+
+	return (1);
+}
